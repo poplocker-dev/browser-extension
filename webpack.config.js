2db9e99dd746946747fe7d46da9de7d9c37a1146
tki <tki.wroc@gmail.com>
2018-10-17 17:30:18 +0200

    add worker-loader to config

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ let config = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.worker\.js$/,
+        use: 'worker-loader'
       }
     ]
   },
