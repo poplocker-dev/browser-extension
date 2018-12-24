305a012af03a0756b96cea105147ca626a14e7ef
Gareth Lewis <garethtlewis@gmail.com>
2018-12-24 17:57:15 +0100

    Change gas price to be one from the tx-sign form

--- src/popup/views/authorize/tx_sign_form.jsx
+++ src/popup/views/authorize/tx_sign_form.jsx
@@ -2,6 +2,7 @@ import React               from 'react'
 import { connect }         from 'react-redux'
 import Button              from 'ui/button'
 import { signTransaction } from 'lib/store/actions'
+import unit                from 'ethjs-unit'
 
 class TxSignForm extends React.Component {
   constructor (props) {
@@ -20,19 +21,20 @@ class TxSignForm extends React.Component {
 }
 
 const mapStore = ({ pending, balance, transaction }) => {
-  const { gasPrice } = transaction;
-  const pendingOne = pending[0].params[0];
+  const gasPrice = unit.toWei(transaction.gasPrice, 'gwei');
+  const params = pending[0].params[0];
 
   return {
-    currentTx: Object.assign(pendingOne,
+    currentTx: Object.assign(params,
                              { gasPrice, balance },
-                             { gasPrice: pendingOne.gasPrice })
+                             { gasPrice: '0x' + gasPrice.toString(16) })
   }
 };
 
 const mapDispatch = (dispatch) => ({
   handleSubmit: function (e) {
     e.preventDefault();
+
     dispatch(signTransaction(this.props.currentTx, this.inputRef.current.value));
   }
 });
