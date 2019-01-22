91d354f93bfc85d448e140b40405a8c6d3e6b717
tki <tki.wroc@gmail.com>
2019-01-22 15:44:25 +0100

    make pager compatible with connected components

--- src/popup/pager.jsx
+++ src/popup/pager.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Pager = (props) => {
-  const el = props.children.find(i => i.type.name == props.page);
+  const el = props.children.find(i => {
+    const name = i.type.displayName || i.type.name;
+    return name.indexOf(props.page) != -1;
+  });
+
   return React.createElement(el.type);
 }
 
