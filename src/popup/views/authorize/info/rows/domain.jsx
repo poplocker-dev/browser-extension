28b758205cc247b4246089588845e165a15f2a4e
tki <tki.wroc@gmail.com>
2019-02-10 23:15:19 +0100

    fix domain advanced mode

--- src/popup/views/authorize/info/rows/domain.jsx
+++ src/popup/views/authorize/info/rows/domain.jsx
@@ -2,14 +2,14 @@ import React       from 'react'
 import Preloader   from 'ui/loader'
 import parseDomain from 'parse-domain'
 
-const Domain = ({ transaction, advanced }) => (
+const Domain = ({ transaction, advancedMode }) => (
   <div className="row domain">
-    <span className="row-label">{advanced? 'To:' : 'From:'}</span>
+    <span className="row-label">{advancedMode? 'To:' : 'From:'}</span>
 
     <Preloader value={transaction.pending.current}>
 
       <div className="amount ellipsis" alt={`from ${domain(transaction)}`}>
-        { advanced? recipient(transaction) : domain(transaction) }
+        { advancedMode? recipient(transaction) : domain(transaction) }
       </div>
 
     </Preloader>
