95db1113079c45c5b2f3fcd125ef1bdd5b7839d4
tki <tki.wroc@gmail.com>
2019-02-11 17:14:56 +0100

    rm resolved listener

--- src/lib/tx.js
+++ src/lib/tx.js
@@ -5,11 +5,12 @@ import { sign as signer } from 'ethjs-signer'
 export function auth (tx) {
   return new Promise(resolve => {
 
-    chrome.runtime.onMessage.addListener(message => {
-      if ( message.type == 'TX_SIGNED')
+    chrome.runtime.onMessage.addListener(function handleSign(message) {
+      if ( message.type == 'TX_SIGNED') {
         resolve(raw.format('eth_sendRawTransaction', [message.tx], message.id));
+        chrome.runtime.onMessage.removeListener(handleSign);
+      }
     });
-
     transaction.add(tx);
   });
 }
