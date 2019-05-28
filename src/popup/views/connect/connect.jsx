f5f7b8b17a1e3df4bac89039809148abd3e5def5
tki <tki.wroc@gmail.com>
2019-05-28 16:16:02 +0200

    style connect view

--- src/popup/views/connect/connect.jsx
+++ src/popup/views/connect/connect.jsx
@@ -4,20 +4,32 @@ import { approve, reject } from 'lib/rpc/connection'
 import { Button }          from '@poplocker/react-ui'
 // import Favicon from './favicon'
 
+import './connect.css'
+
 class ConnectView extends React.Component  {
   render () {
     return (
-      <div className="view connect-view">
-        {/* <Favicon url={this.props.request}/> */}
-        <Button icon="tick"
-                onClick={() => approve(this.props.request)}>
-          Connect
-        </Button>
-        <Button kind="reject"
-                icon="close"
-                onClick={() => reject(this.props.request)}>
-          Reject
-        </Button>
+      <div className="view connect">
+        <div className="connect_favicon">
+          {/* <Favicon url={this.props.request}/> */}
+        </div>
+        <p className="connect_hostname">
+          { this.props.request }
+        </p>
+        <p className="connect_caption">
+          Wants to connect with your account
+        </p>
+        <div className="buttons buttons--2row">
+          <Button icon="tick"
+                  onClick={() => approve(this.props.request)}>
+            Connect
+          </Button>
+          <Button kind="reject"
+                  icon="close"
+                  onClick={() => reject(this.props.request)}>
+            Reject
+          </Button>
+        </div>
       </div>
     )
   }
